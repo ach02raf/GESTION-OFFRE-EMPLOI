e0c624c0ce734ae70b24b37ae2b0be0d3b9b558e
ach02raf <ach02raf@gmail.com>
2023-12-05 21:25:24 +0100

    5-12-2023

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'DMWM-GESTION-OFFRE-EMPLOI';
+  title = 'GESTION-OFFRE-EMPLOI';
 }
